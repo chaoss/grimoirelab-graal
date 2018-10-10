f9b56f3bb79372f25c96000258c20241a3a8db7f
Valerio Cosentino <valcos@bitergia.com>
2018-10-10 17:26:13 +0200

    [colic] Add support for scancode analyzer
    This code extends the colic backend by adding support for the
    scancode analyzer.  Currently, the backend supports nomos and
    scancode analyzers, they can be activated by passing the
    corresponding category (`code_license_nomos`,
    `code_license_scancode`).
    Note that the backend requires the local path where the
    executable is located.

--- graal/backends/core/colic.py
+++ graal/backends/core/colic.py
@@ -23,13 +23,19 @@
 import logging
 
 from graal.graal import (Graal,
+                         GraalError,
                          GraalRepository,
                          GraalCommand,
                          DEFAULT_WORKTREE_PATH)
 from graal.backends.core.analyzers.nomos import Nomos
+from graal.backends.core.analyzers.scancode import ScanCode
 from perceval.utils import DEFAULT_DATETIME, DEFAULT_LAST_DATETIME
 
-CATEGORY_COLIC = 'code_license'
+NOMOS = 'nomos'
+SCANCODE = 'scancode'
+
+CATEGORY_COLIC_NOMOS = 'code_license_' + NOMOS
+CATEGORY_COLIC_SCANCODE = 'code_license_' + SCANCODE
 
 logger = logging.getLogger(__name__)
 
@@ -41,7 +47,8 @@ class CoLic(Graal):
     using Nomos
 
     :param uri: URI of the Git repository
-    :param gitpath: path to the repository or to the log file
+    :param git_path: path to the repository or to the log file
+    :param exec_path: path of the executable
     :param worktreepath: the directory where to store the working tree
     :param entrypoint: the entrypoint of the analysis
     :param in_paths: the target paths of the analysis
@@ -52,22 +59,37 @@ class CoLic(Graal):
     :raises RepositoryError: raised when there was an error cloning or
         updating the repository.
     """
-    version = '0.2.2'
+    version = '0.4.0'
 
-    CATEGORIES = [CATEGORY_COLIC]
+    CATEGORIES = [CATEGORY_COLIC_NOMOS, CATEGORY_COLIC_SCANCODE]
 
-    def __init__(self, uri, git_path, worktreepath=DEFAULT_WORKTREE_PATH,
+    def __init__(self, uri, git_path, exec_path, worktreepath=DEFAULT_WORKTREE_PATH,
                  entrypoint=None, in_paths=None, out_paths=None,
                  tag=None, archive=None):
         super().__init__(uri, git_path, worktreepath,
                          entrypoint=entrypoint, in_paths=in_paths, out_paths=out_paths,
                          tag=tag, archive=archive)
-        self.license_analyzer = LicenseAnalyzer()
 
-    def fetch(self, category=CATEGORY_COLIC, paths=None,
+        if not GraalRepository.exists(exec_path):
+            raise GraalError(cause="executable path %s not valid" % exec_path)
+
+        self.exec_path = exec_path
+        self.analyzer_kind = None
+        self.analyzer = None
+
+    def fetch(self, category=CATEGORY_COLIC_NOMOS, paths=None,
               from_date=DEFAULT_DATETIME, to_date=DEFAULT_LAST_DATETIME,
               branches=None, latest_items=False):
         """Fetch commits and add license information."""
+
+        if category == CATEGORY_COLIC_SCANCODE:
+            self.analyzer_kind = SCANCODE
+        elif category == CATEGORY_COLIC_NOMOS:
+            self.analyzer_kind = NOMOS
+        else:
+            raise GraalError(cause="Unknown category %s" % category)
+
+        self.analyzer = LicenseAnalyzer(self.exec_path, self.analyzer_kind)
 
         items = super().fetch(category,
                               from_date=from_date, to_date=to_date,
@@ -79,10 +101,15 @@ class CoLic(Graal):
     def metadata_category(item):
         """Extracts the category from a Code item.
 
-        This backend only generates one type of item which is
-        'code_complexity'.
+        This backend generates two types of item which can be:
+        'code_license_nomos' or 'code_license_scancode'.
         """
-        return CATEGORY_COLIC
+        if item['analyzer'] == NOMOS:
+            return CATEGORY_COLIC_NOMOS
+        elif item['analyzer'] == SCANCODE:
+            return CATEGORY_COLIC_SCANCODE
+        else:
+            raise GraalError(cause="Unknown analyzer %s" % item['analyzer'])
 
     def _filter_commit(self, commit):
         """Filter a commit according to its data (e.g., author, sha, etc.)
@@ -107,18 +134,22 @@ class CoLic(Graal):
 
         :param commit: a Perceval commit item
         """
-        files = GraalRepository.files(self.worktreepath)
         analysis = []
 
-        for file_path in files:
+        for committed_file in commit['files']:
 
+            file_path = committed_file['file']
             if self.in_paths:
                 found = [p for p in self.in_paths if file_path.endswith(p)]
                 if not found:
                     continue
 
-            license_info = self.license_analyzer.analyze(file_path)
-            license_info.update({'file_path': file_path.replace(self.worktreepath + '/', "")})
+            local_path = self.worktreepath + '/' + file_path
+            if not GraalRepository.exists(local_path):
+                continue
+
+            license_info = self.analyzer.analyze(local_path)
+            license_info.update({'file_path': file_path})
             analysis.append(license_info)
 
         return analysis
@@ -131,14 +162,22 @@ class CoLic(Graal):
         commit.pop('files', None)
         commit.pop('parents', None)
         commit.pop('refs', None)
+        commit['analyzer'] = self.analyzer_kind
         return commit
 
 
 class LicenseAnalyzer:
-    """Class to analyse the content of files"""
+    """Class to analyse the content of files
 
-    def __init__(self):
-        self.nomos = Nomos()
+    :param exec_path: path of the license analyzer executable
+    :param kind: the analyzer kind (e.g., NOMOS, SCANCODE)
+    """
+
+    def __init__(self, exec_path, kind=NOMOS):
+        if kind == SCANCODE:
+            self.analyzer = ScanCode(exec_path)
+        else:
+            self.analyzer = Nomos(exec_path)
 
     def analyze(self, file_path):
         """Analyze the content of a file using Nomos
@@ -151,7 +190,7 @@ class LicenseAnalyzer:
         }
         """
         kwargs = {'file_path': file_path}
-        analysis = self.nomos.analyze(**kwargs)
+        analysis = self.analyzer.analyze(**kwargs)
 
         return analysis
 
@@ -160,3 +199,11 @@ class CoLicCommand(GraalCommand):
     """Class to run CoLic backend from the command line."""
 
     BACKEND = CoLic
+
+    @staticmethod
+    def setup_cmd_parser():
+        """Returns the CoLic argument parser."""
+
+        parser = GraalCommand.setup_cmd_parser(exec_path=True)
+
+        return parser
