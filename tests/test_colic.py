17a94a25382d34e3a50d771b79afc949b69db764
Valerio Cosentino <valcos@bitergia.com>
2018-10-10 17:38:00 +0200

    [tests] Update tests for colic backend
    This code aligns the tests for the colic backend, which now can
    rely on two different analyzers (nomos and scancode).

--- tests/test_colic.py
+++ tests/test_colic.py
@@ -27,14 +27,25 @@ import subprocess
 import tempfile
 import unittest.mock
 
+from grimoirelab_toolkit.datetime import str_to_datetime
+from graal.graal import (GraalCommandArgumentParser,
+                         GraalError)
 from graal.backends.core.analyzers.nomos import Nomos
-from graal.backends.core.colic import (CATEGORY_COLIC,
+from graal.backends.core.analyzers.scancode import ScanCode
+from graal.backends.core.colic import (CATEGORY_COLIC_NOMOS,
+                                       CATEGORY_COLIC_SCANCODE,
+                                       NOMOS,
+                                       SCANCODE,
                                        CoLic,
                                        LicenseAnalyzer,
                                        CoLicCommand)
 from test_graal import TestCaseGraal
 from base_analyzer import (ANALYZER_TEST_FILE,
                            TestCaseAnalyzer)
+
+
+NOMOS_PATH = "/home/slimbook/Escritorio/sources/graal-libs/nomossa"
+SCANCODE_PATH = "/home/slimbook/Escritorio/sources/graal-libs/scancode-toolkit-2.9.2/scancode"
 
 
 class TestCoLicBackend(TestCaseGraal):
@@ -71,35 +82,49 @@ class TestCoLicBackend(TestCaseGraal):
     def test_initialization(self):
         """Test whether attributes are initializated"""
 
-        cl = CoLic('http://example.com', self.git_path, self.worktree_path, tag='test')
+        cl = CoLic('http://example.com', self.git_path, NOMOS_PATH, self.worktree_path, tag='test')
         self.assertEqual(cl.uri, 'http://example.com')
         self.assertEqual(cl.gitpath, self.git_path)
         self.assertEqual(cl.worktreepath, os.path.join(self.worktree_path, os.path.split(cl.gitpath)[1]))
         self.assertEqual(cl.origin, 'http://example.com')
         self.assertEqual(cl.tag, 'test')
-        self.assertIsInstance(cl.license_analyzer, LicenseAnalyzer)
+        self.assertEqual(cl.exec_path, NOMOS_PATH)
+        self.assertIsNone(cl.analyzer)
+        self.assertIsNone(cl.analyzer_kind)
 
-        cl = CoLic('http://example.com', self.git_path, self.worktree_path, tag='test')
+        cl = CoLic('http://example.com', self.git_path, NOMOS_PATH, self.worktree_path, tag='test')
         self.assertEqual(cl.uri, 'http://example.com')
         self.assertEqual(cl.gitpath, self.git_path)
         self.assertEqual(cl.worktreepath, os.path.join(self.worktree_path, os.path.split(cl.gitpath)[1]))
         self.assertEqual(cl.origin, 'http://example.com')
         self.assertEqual(cl.tag, 'test')
-        self.assertIsInstance(cl.license_analyzer, LicenseAnalyzer)
+        self.assertEqual(cl.exec_path, NOMOS_PATH)
+        self.assertIsNone(cl.analyzer)
+        self.assertIsNone(cl.analyzer_kind)
 
         # When tag is empty or None it will be set to the value in uri
-        cl = CoLic('http://example.com', self.git_path, self.worktree_path)
+        cl = CoLic('http://example.com', self.git_path, NOMOS_PATH, self.worktree_path)
         self.assertEqual(cl.origin, 'http://example.com')
         self.assertEqual(cl.tag, 'http://example.com')
+        self.assertEqual(cl.exec_path, NOMOS_PATH)
+        self.assertIsNone(cl.analyzer)
+        self.assertIsNone(cl.analyzer_kind)
 
-        cl = CoLic('http://example.com', self.git_path, self.worktree_path)
+        cl = CoLic('http://example.com', self.git_path, NOMOS_PATH, self.worktree_path)
         self.assertEqual(cl.origin, 'http://example.com')
         self.assertEqual(cl.tag, 'http://example.com')
+        self.assertEqual(cl.exec_path, NOMOS_PATH)
+        self.assertIsNone(cl.analyzer)
+        self.assertIsNone(cl.analyzer_kind)
 
-    def test_fetch(self):
+        with self.assertRaises(GraalError):
+            _ = CoLic('http://example.com', self.git_path, "/tmp/invalid", worktreepath=self.worktree_path)
+
+    def test_fetch_nomossa(self):
         """Test whether commits are properly processed"""
 
-        cl = CoLic('http://example.com', self.git_path, self.worktree_path, in_paths=['perceval/backends/core/github.py'])
+        cl = CoLic('http://example.com', self.git_path, NOMOS_PATH, self.worktree_path,
+                   in_paths=['perceval/backends/core/github.py'])
         commits = [commit for commit in cl.fetch()]
 
         self.assertEqual(len(commits), 1)
@@ -107,7 +132,7 @@ class TestCoLicBackend(TestCaseGraal):
 
         for commit in commits:
             self.assertEqual(commit['backend_name'], 'CoLic')
-            self.assertEqual(commit['category'], CATEGORY_COLIC)
+            self.assertEqual(commit['category'], CATEGORY_COLIC_NOMOS)
             self.assertEqual(commit['data']['analysis'][0]['file_path'],
                              'perceval/backends/core/github.py')
             self.assertTrue('Author' in commit['data'])
@@ -116,6 +141,67 @@ class TestCoLicBackend(TestCaseGraal):
             self.assertFalse('parents' in commit['data'])
             self.assertFalse('refs' in commit['data'])
 
+    def test_fetch_scancode(self):
+        """Test whether commits are properly processed"""
+
+        cl = CoLic('http://example.com', self.git_path, SCANCODE_PATH, self.worktree_path,
+                   in_paths=['perceval/backends/core/github.py'])
+        commits = [commit for commit in cl.fetch(category=CATEGORY_COLIC_SCANCODE)]
+
+        self.assertEqual(len(commits), 1)
+        self.assertFalse(os.path.exists(cl.worktreepath))
+
+        for commit in commits:
+            self.assertEqual(commit['backend_name'], 'CoLic')
+            self.assertEqual(commit['category'], CATEGORY_COLIC_SCANCODE)
+            self.assertEqual(commit['data']['analysis'][0]['file_path'],
+                             'perceval/backends/core/github.py')
+            self.assertTrue('Author' in commit['data'])
+            self.assertTrue('Commit' in commit['data'])
+            self.assertFalse('files' in commit['data'])
+            self.assertFalse('parents' in commit['data'])
+            self.assertFalse('refs' in commit['data'])
+
+    def test_metadata_category(self):
+        """Test metadata_category"""
+
+        item = {
+            "Author": "Valerio Cosentino <valcos@bitergia.com>",
+            "AuthorDate": "Fri May 18 18:26:48 2018 +0200",
+            "Commit": "Valerio Cosentino <valcos@bitergia.com>",
+            "CommitDate": "Fri May 18 18:26:48 2018 +0200",
+            "analysis": [],
+            "analyzer": "scancode",
+            "commit": "075f0c6161db5a3b1c8eca45e08b88469bb148b9",
+            "message": "[perceval] first commit"
+        }
+        self.assertEqual(CoLic.metadata_category(item), CATEGORY_COLIC_SCANCODE)
+
+        item = {
+            "Author": "Valerio Cosentino <valcos@bitergia.com>",
+            "AuthorDate": "Fri May 18 18:26:48 2018 +0200",
+            "Commit": "Valerio Cosentino <valcos@bitergia.com>",
+            "CommitDate": "Fri May 18 18:26:48 2018 +0200",
+            "analysis": [],
+            "analyzer": "nomos",
+            "commit": "075f0c6161db5a3b1c8eca45e08b88469bb148b9",
+            "message": "[perceval] first commit"
+        }
+        self.assertEqual(CoLic.metadata_category(item), CATEGORY_COLIC_NOMOS)
+
+        item = {
+            "Author": "Valerio Cosentino <valcos@bitergia.com>",
+            "AuthorDate": "Fri May 18 18:26:48 2018 +0200",
+            "Commit": "Valerio Cosentino <valcos@bitergia.com>",
+            "CommitDate": "Fri May 18 18:26:48 2018 +0200",
+            "analysis": [],
+            "analyzer": "unknown",
+            "commit": "075f0c6161db5a3b1c8eca45e08b88469bb148b9",
+            "message": "[perceval] first commit"
+        }
+        with self.assertRaises(GraalError):
+            _ = CoLic.metadata_category(item)
+
 
 class TestLicenseAnalyzer(TestCaseAnalyzer):
     """LicenseAnalyzer tests"""
@@ -123,16 +209,32 @@ class TestLicenseAnalyzer(TestCaseAnalyzer):
     def test_init(self):
         """Test initialization"""
 
-        license_analyzer = LicenseAnalyzer()
-
+        license_analyzer = LicenseAnalyzer(NOMOS_PATH)
         self.assertIsInstance(license_analyzer, LicenseAnalyzer)
-        self.assertIsInstance(license_analyzer.nomos, Nomos)
+        self.assertIsInstance(license_analyzer.analyzer, Nomos)
+
+        license_analyzer = LicenseAnalyzer(NOMOS_PATH, NOMOS)
+        self.assertIsInstance(license_analyzer, LicenseAnalyzer)
+        self.assertIsInstance(license_analyzer.analyzer, Nomos)
+
+        license_analyzer = LicenseAnalyzer(SCANCODE_PATH, SCANCODE)
+        self.assertIsInstance(license_analyzer, LicenseAnalyzer)
+        self.assertIsInstance(license_analyzer.analyzer, ScanCode)
+
+        with self.assertRaises(GraalError):
+            _ = LicenseAnalyzer("/tmp/analyzer", SCANCODE)
 
     def test_analyze(self):
         """Test whether the analyze method works"""
 
         file_path = os.path.join(self.tmp_data_path, ANALYZER_TEST_FILE)
-        license_analyzer = LicenseAnalyzer()
+        license_analyzer = LicenseAnalyzer(NOMOS_PATH)
+        analysis = license_analyzer.analyze(file_path)
+
+        self.assertIn('licenses', analysis)
+
+        file_path = os.path.join(self.tmp_data_path, ANALYZER_TEST_FILE)
+        license_analyzer = LicenseAnalyzer(SCANCODE_PATH, kind=SCANCODE)
         analysis = license_analyzer.analyze(file_path)
 
         self.assertIn('licenses', analysis)
@@ -145,6 +247,26 @@ class TestCoLicCommand(unittest.TestCase):
         """Test if the backend class is CoLic"""
 
         self.assertIs(CoLicCommand.BACKEND, CoLic)
+
+    def test_setup_cmd_parser(self):
+        """Test setup_cmd_parser"""
+
+        parser = CoLicCommand.setup_cmd_parser()
+
+        self.assertIsInstance(parser, GraalCommandArgumentParser)
+
+        args = ['http://example.com/',
+                '--git-path', '/tmp/gitpath',
+                '--tag', 'test',
+                '--from-date', '1975-01-01',
+                '--exec-path', '/tmp/execpath']
+
+        parsed_args = parser.parse(*args)
+        self.assertEqual(parsed_args.uri, 'http://example.com/')
+        self.assertEqual(parsed_args.git_path, '/tmp/gitpath')
+        self.assertEqual(parsed_args.tag, 'test')
+        self.assertEqual(parsed_args.from_date, str_to_datetime('1975-01-01'))
+        self.assertEqual(parsed_args.exec_path, '/tmp/execpath')
 
 
 if __name__ == "__main__":
