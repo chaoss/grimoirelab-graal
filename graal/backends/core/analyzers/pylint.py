0909e77045f90dbe2c79d8704c25502f1d059832
Sanjana Nayar <sanjananayar2k1@gmail.com>
2020-03-19 01:12:04 +0530

    [graal] Update license and copyright information
    This commit updates license information for every source
    code file in Graal. The author names are also updated
    using a script
    
    Signed-off-by: Sanjana Nayar <sanjananayar2k1@gmail.com>

--- graal/backends/core/analyzers/pylint.py
+++ graal/backends/core/analyzers/pylint.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*- the Graal backend.
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 #
-# Copyright (C) 2015-2019 Bitergia
+# Copyright (C) 2015-2020 Bitergia
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,6 +17,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 # Authors:
+#     inishchith <inishchith@gmail.com>
 #     Valerio Cosentino <valcos@bitergia.com>
 #
 
