ce0cdc18897738ac2f7e8f62f4e41c60e7d0251b
Willem Meijer <w.meijer.5@student.rug.nl>
2022-01-17 12:50:13 +0100

    licenses and versions

--- graal/backends/core/cocom/cocom_analyzer_factory.py
+++ graal/backends/core/cocom/cocom_analyzer_factory.py
@@ -1,7 +1,27 @@
-from socket import IPPORT_RESERVED
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2020 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     wmeijer221 <w.meijer.5@student.rug.nl>
+#
+
 from graal.backends.core.composer import Composer
 from graal.graal import GraalError
-from graal.util import create_instances
 
 from graal.backends.core.cocom.compositions.composition_lizard_file import *
 from graal.backends.core.cocom.compositions.composition_lizard_repository import *
@@ -12,15 +32,17 @@ from graal.backends.core.cocom.compositions.composition_scc_repository import *
 class CoComAnalyzerFactory:
     """Factory class for Analyzer Compositions"""
 
+    version = '0.1.0'
+
     def __init__(self):
         self._load_compositions()
 
     def _load_compositions(self):
-        #TODO: add dynamic loading.
-        #TODO: do something about kind + category
+        # TODO: add dynamic loading.
+        # TODO: do something about kind + category
         self.compositions = {
             CATEGORY_COCOM_LIZARD_FILE: CompositionLizardFile(),
-            LIZARD_FILE: CompositionLizardFile(), 
+            LIZARD_FILE: CompositionLizardFile(),
             CATEGORY_COCOM_LIZARD_REPOSITORY: CompositionLizardRepository(),
             LIZARD_REPOSITORY: CompositionLizardRepository(),
             CATEGORY_COCOM_SCC_FILE: CompositionSccFile(),
--- graal/backends/core/cocom/compositions/composition_scc_repository.py
+++ graal/backends/core/cocom/compositions/composition_scc_repository.py
@@ -1,3 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2020 Bitergia
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+#     wmeijer221 <w.meijer.5@student.rug.nl>
+#
+
 from graal.backends.core.analyzers.scc import SCC
 from graal.backends.core.composer import Composer
 
@@ -7,6 +29,8 @@ CATEGORY_COCOM_SCC_REPOSITORY = 'code_complexity_' + SCC_REPOSITORY
 
 class CompositionSccRepository(Composer):
     """Analyzer Composition for Lizard Files"""
+
+    version = '0.1.0'
 
     def get_composition(self):
         return [SCC()]
